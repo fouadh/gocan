86125513e36417d5547b57b30449deaf2d96ae6f
Fouad Hamdi <1621862+fouadh@users.noreply.github.com>
2021-05-27 13:45:12 +0200

    introduce a create app usecase

--- internal/create-app/command.go
+++ internal/create-app/command.go
@@ -2,9 +2,6 @@ package create_app
 
 import (
 	context "com.fha.gocan/internal/platform"
-	"fmt"
-	"github.com/pborman/uuid"
-	"github.com/pkg/errors"
 	"github.com/spf13/cobra"
 )
 
@@ -15,21 +12,15 @@ func NewCommand(ctx *context.Context) *cobra.Command {
 		Use: "create-app",
 		Args: cobra.ExactArgs(1),
 		RunE: func(cmd *cobra.Command, args []string) error {
-			datasource := ctx.DataSource
-			connection, err := datasource.GetConnection()
-			if err != nil {
-				return errors.Wrap(err, fmt.Sprintf("The connection to the dabase could not be established: %v", err.Error()))
+			request := CreateAppRequest{
+				Name:      args[0],
+				SceneName: sceneName,
 			}
-			id := uuid.NewUUID().String()
-			_, err = connection.Exec("insert into apps(id, name, scene_id) values($1, $2, (select id from scenes where name=$3))", id, args[0], sceneName)
-			if err != nil {
-				return errors.Wrap(err, "App could not be created")
-			} else {
-				return err
-			}
-			return nil
+
+			return CreateApp(request, ctx)
 		},
 	}
 	cmd.Flags().StringVarP(&sceneName, "scene", "s", "", "Scene name")
 	return &cmd
 }
+
---
+++ internal/create-app/usecase.go
@@ -0,0 +1,29 @@
+package create_app
+
+import (
+	"com.fha.gocan/internal/platform"
+	"fmt"
+	"github.com/pborman/uuid"
+	"github.com/pkg/errors"
+)
+
+type CreateAppRequest struct {
+	Name      string
+	SceneName string
+}
+
+func CreateApp(request CreateAppRequest, ctx *context.Context) error {
+	datasource := ctx.DataSource
+	connection, err := datasource.GetConnection()
+	if err != nil {
+		return errors.Wrap(err, fmt.Sprintf("The connection to the dabase could not be established: %v", err.Error()))
+	}
+	id := uuid.NewUUID().String()
+	_, err = connection.Exec("insert into apps(id, name, scene_id) values($1, $2, (select id from scenes where name=$3))", id, request.Name, request.SceneName)
+	if err != nil {
+		return errors.Wrap(err, "App could not be created")
+	} else {
+		return err
+	}
+	return nil
+}
