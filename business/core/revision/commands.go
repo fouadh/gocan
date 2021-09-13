a516215a906eb83ce575bde70878a2206a692bf3
Fouad Hamdi <1621862+fouadh@users.noreply.github.com>
2021-09-13 09:31:34 +0200

    support date flags for the revisions command

--- business/core/revision/commands.go
+++ business/core/revision/commands.go
@@ -3,7 +3,6 @@ package revision
 import (
 	context "com.fha.gocan/foundation"
 	"fmt"
-	"github.com/jmoiron/sqlx"
 	"github.com/pkg/errors"
 	"github.com/spf13/cobra"
 	"time"
@@ -11,6 +10,8 @@ import (
 
 func NewRevisionsCommand(ctx *context.Context) *cobra.Command {
 	var sceneName string
+	var before string
+	var after string
 
 	cmd := cobra.Command{
 		Use:  "revisions",
@@ -26,45 +27,15 @@ func NewRevisionsCommand(ctx *context.Context) *cobra.Command {
 			}
 
 			appName := args[0]
-			/*appId, err := getAppId(appName, connection, sceneName)
-			if err != nil {
-				return err
-			}
-
-			sql := fmt.Sprintf(`select entity,
-				   numberOfRevisions,
-				   numberOfAuthors,
-				   cast(numberOfRevisions as decimal) / MAX(numberOfRevisions) OVER () as normalizedNumberOfRevisions,
-				   coalesce((select lines from cloc where file=entity and app_id=$1), 0) as code
-					FROM (
-					 SELECT file     as entity,
-								count(distinct c.author) as numberOfAuthors,
-							COUNT(*) as numberOfRevisions
-					 FROM stats inner join commits c on c.id = stats.commit_id
-					 and c.date between $3 and $2
-					  WHERE stats.app_id=$1
-					  AND FILE NOT LIKE '%%=>%%'
-					 GROUP BY file
-				 ) a
-				ORDER BY %s desc
-			`, "numberOfRevisions")
-
-			revisions := []revision.Revision{}
-			before := time.Now().AddDate(0, 0, 1)
-			after, _ := time.Parse("2006-01-02", "1970-01-01")
-
-			err = connection.Select(&revisions, sql, appId, before, after)
-
-			if err != nil {
-				ui.Failed("Cannot fetch revisions: " + err.Error())
-				return err
-			}*/
 
 			core := NewCore(connection)
-			before := time.Now().AddDate(0, 0, 1)
-			after, _ := time.Parse("2006-01-02", "1970-01-01")
+			beforeTime := time.Now().AddDate(0, 0, 1)
+			if after == "" {
+				after = "1970-01-01"
+			}
+			afterTime, _ := time.Parse("2006-01-02", after)
 
-			revisions, err := core.GetRevisions(*ctx, appName, sceneName, before, after)
+			revisions, err := core.GetRevisions(*ctx, appName, sceneName, beforeTime, afterTime)
 
 			if err != nil {
 				ui.Failed("Cannot fetch revisions: " + err.Error())
@@ -86,16 +57,7 @@ func NewRevisionsCommand(ctx *context.Context) *cobra.Command {
 	}
 
 	cmd.Flags().StringVarP(&sceneName, "scene", "s", "", "Scene name")
+	cmd.Flags().StringVarP(&before, "before", "", "", "")
+	cmd.Flags().StringVarP(&after, "after", "", "", "")
 	return &cmd
-}
-
-func getAppId(appName string, connection *sqlx.DB, sceneName string) (string, error) {
-	appId := ""
-	if err := connection.Get(&appId, "select id from apps where name=$1 and scene_id=(select id from scenes where name=$2)", appName, sceneName); err != nil {
-		return "", errors.Wrap(err, "Unable to retrieve matching app id")
-	}
-	if appId == "" {
-		return "", errors.New("Application not found.")
-	}
-	return appId, nil
 }
