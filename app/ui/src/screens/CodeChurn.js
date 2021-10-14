161e5ba612a93166ee07ef5943cb603e7f83e3b5
Fouad Hamdi <1621862+fouadh@users.noreply.github.com>
2021-10-14 16:06:26 +0200

    add a date selector in the code churn screen

--- app/ui/src/screens/CodeChurn.js
+++ app/ui/src/screens/CodeChurn.js
@@ -2,29 +2,50 @@ import {useEffect, useState} from "react";
 import axios from "axios";
 import * as d3 from "d3";
 import {MultiLineChart} from "../components/MultiLineChart";
+import {DateSelector} from "../components/DateSelector";
 
 export function CodeChurn({sceneId, appId}) {
+    const [dateRange, setDateRange] = useState({});
+    const [analyze, setAnalyze] = useState(true);
     const [codeChurn, setCodeChurn] = useState([]);
 
     useEffect(() => {
         let subscribed = true;
-        axios.get(`/api/scenes/${sceneId}/apps/${appId}/code-churn`)
-            .then(it => it.data)
-            .then(it => it.codeChurn)
-            .then(it => {
-                if (subscribed) {
-                    const added = it.map((each) => ({x: each.date, y: each.added}));
-                    const deleted = it.map((each) => ({x: each.date, y: each.deleted}));
-                    const churn = [added, deleted];
-                    setCodeChurn(churn);
+
+        if (analyze) {
+            let endpoint = `/api/scenes/${sceneId}/apps/${appId}/code-churn`;
+            if (dateRange.min) {
+                if (dateRange.max) {
+                    endpoint += `?after=${dateRange.min}&before=${dateRange.max}`
+                } else {
+                    endpoint += `?after=${dateRange.min}`
                 }
-            });
+            } else if (dateRange.max) {
+                endpoint += `?before=${dateRange.max}`
+            }
+            axios.get(endpoint)
+                .then(it => it.data)
+                .then(it => it.codeChurn)
+                .then(it => {
+                    if (subscribed) {
+                        const added = it.map((each) => ({x: each.date, y: each.added}));
+                        const deleted = it.map((each) => ({x: each.date, y: each.deleted}));
+                        const churn = [added, deleted];
+                        setCodeChurn(churn);
+                    }
+                })
+                .finally(() => {
+                    setAnalyze(false);
+                });
+        }
 
         return () => subscribed = false;
-    }, [sceneId, appId]);
+    }, [sceneId, appId, analyze]);
 
     return (
         <>
+            <DateSelector onChange={e => setDateRange(e)}/>
+            <button onClick={e => setAnalyze(true)}>Submit</button>
             <MultiLineChart yLabel="Code Churn"
                             data={codeChurn}
                             xAccessor={d => d3.timeParse('%Y-%m-%d')(d.x)}
