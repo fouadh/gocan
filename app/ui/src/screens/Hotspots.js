0fd0903dee2d9735ef461e2a35b11ec03a595dc3
Fouad Hamdi <1621862+fouadh@users.noreply.github.com>
2021-10-14 11:24:36 +0200

    allow to select a date range to visualize hotspots

--- app/ui/src/screens/Hotspots.js
+++ app/ui/src/screens/Hotspots.js
@@ -4,23 +4,39 @@ import {CirclePacking} from "../components/CirclePacking";
 import {Spinner} from "../components/Spinner";
 
 export function Hotspots({sceneId, appId}) {
+    const [dateRange, setDateRange] = useState({});
     const [hospots, setHotspots] = useState();
     const [loading, setLoading] = useState(false);
+    const [analyze, setAnalyze] = useState(false);
 
     useEffect(() => {
         let subscribed = true;
         setLoading(true);
-        axios.get(`/api/scenes/${sceneId}/apps/${appId}/hotspots`)
+        let endpoint = `/api/scenes/${sceneId}/apps/${appId}/hotspots` ;
+        if (dateRange.min) {
+            if (dateRange.max) {
+                endpoint += `?after=${dateRange.min}&before=${dateRange.max}`
+            } else {
+                endpoint += `?after=${dateRange.min}`
+            }
+        } else if (dateRange.max) {
+            endpoint += `?before=${dateRange.max}`
+        }
+        console.log({dateRange});
+        axios.get(endpoint)
             .then(it => it.data)
             .then(it => {
                 if (subscribed) {
                     setHotspots(it);
                 }
-            }).finally(() => setLoading(false));
+            }).finally(() => {
+            setLoading(false);
+            setAnalyze(false);
+        });
         ;
 
         return () => subscribed = false;
-    }, [sceneId, appId]);
+    }, [sceneId, appId, analyze]);
 
     let screen;
 
@@ -34,5 +50,14 @@ export function Hotspots({sceneId, appId}) {
         screen = <><p>Unable to get hotspots</p></>
     }
 
-    return <>{screen}</>
+    return <>
+        <div>
+            <label htmlFor="min">Min Date:</label>
+            <input type="text" value={dateRange.min} onChange={e => setDateRange({...dateRange, min: e.target.value})}/>
+            <label htmlFor="max">Max Date:</label>
+            <input type="text" value={dateRange.max} onChange={e => setDateRange({...dateRange, max: e.target.value})}/>
+            <button onClick={e => setAnalyze(true)}>Submit</button>
+        </div>
+        {screen}
+    </>
 }
