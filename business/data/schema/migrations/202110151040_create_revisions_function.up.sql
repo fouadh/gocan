5b79c0db4189c255c763c145090ef913ca06faa0
Fouad Hamdi <1621862+fouadh@users.noreply.github.com>
2021-10-15 11:09:38 +0200

    select cloc based on the specified revision dates

---
+++ business/data/schema/migrations/202110151040_create_revisions_function.up.sql
@@ -0,0 +1,40 @@
+create or replace function revisions(app_id uuid, before timestamp, after timestamp)
+    returns table
+            (
+                entity                      text,
+                numberOfRevisions           bigint,
+                numberOfAuthors             bigint,
+                normalizedNumberOfRevisions decimal,
+                code                        integer
+            )
+as
+$body$
+select entity,
+       numberOfRevisions,
+       numberOfAuthors,
+       cast(numberOfRevisions as decimal) / MAX(numberOfRevisions) OVER () as normalizedNumberOfRevisions,
+       coalesce((
+                    select lines
+                    from cloc
+                             inner join commits c2 on cloc.commit_id = c2.id
+                    where cloc.file = entity
+                      and cloc.app_id = $1
+                      and c2.date >= $2
+                    order by c2.date asc
+                    limit 1
+                ), 0)                                                      as code
+FROM (
+         select file                     as entity,
+                count(distinct c.author) as numberOfAuthors,
+                COUNT(*)                 as numberOfRevisions
+         from stats
+                  inner join commits c on c.id = stats.commit_id
+             and c.app_id = stats.app_id
+             and stats.app_id = $1
+             and c.date between $3 and $2
+         WHERE FILE NOT LIKE '%%=>%%'
+         group by stats.app_id, file
+     ) q
+ORDER BY numberOfRevisions desc
+$body$
+    language sql;
