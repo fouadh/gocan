4c458608ccb47e933b9ceb3f3eb42764b9f64aa2
Fouad Hamdi <1621862+fouadh@users.noreply.github.com>
2021-10-20 23:02:09 +0200

    add sample script for Craft.Net analysis

---
+++ doc/samples/craft-sample.sh
@@ -0,0 +1,34 @@
+#!/usr/bin/env bash
+
+set -ex
+
+if [[ -z "${GOCAN}" ]]; then
+  GOCAN="gocan"
+fi
+
+$GOCAN --version
+
+echo "Cleaning up before starting"
+$GOCAN delete-scene craft
+
+echo "Creating the scene & the app"
+$GOCAN create-scene craft
+$GOCAN create-app craft --scene craft
+
+echo "Importing the history"
+tmp_dir=$(mktemp -d -t gocan-)
+echo "Cloning craft in $tmp_dir/craft"
+git clone https://github.com/SirCmpwn/Craft.Net.git $tmp_dir/craft
+
+cd $tmp_dir/craft
+$GOCAN import-history craft --scene craft --before 2013-01-01
+$GOCAN import-history craft --scene craft --before 2014-08-08 --after 2013-01-01
+
+echo "Sum of coupling before 2014-08-08"
+$GOCAN soc craft --scene craft | head -n 10
+
+echo "Analyze coupling of MinecraftServer before 2013-01-01"
+$GOCAN coupling craft --scene craft --before 2013-01-01 --min-degree 40 | grep MinecraftServer.cs
+
+echo "Analyze coupling of MinecraftServer between 2013-01-01 and 2014-08-08"
+$GOCAN coupling craft --scene craft --after 2013-01-01 --min-degree 40 | grep MinecraftServer.cs
