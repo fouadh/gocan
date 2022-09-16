3cdc2893ccf6016e25f81d55944d7e09571d4262
Fouad Hamdi <1621862+fouadh@users.noreply.github.com>
2022-09-16 11:36:55 +0200

    extract module selector in its own file

--- app/ui/src/screens/Hotspots.js
+++ app/ui/src/screens/Hotspots.js
@@ -5,54 +5,7 @@ import {Spinner} from "../components/Spinner";
 import {DateSelector} from "../components/DateSelector";
 import {Button} from 'primereact/button';
 import {BoundarySelector} from "./BoundarySelector";
-import {Dropdown} from "primereact/dropdown";
-
-function ModuleSelector({appId, sceneId, boundaryName, onChange}) {
-    const [module, setModule] = useState()
-    const [modules, setModules] = useState([]);
-
-    useEffect(() => {
-        let subscribed = true;
-        axios.get(`/api/scenes/${sceneId}/apps/${appId}/boundaries/${boundaryName}/modules`)
-            .then(it => it.data)
-            .then(it => it.modules)
-            .then((it) => {
-                if (subscribed) {
-                    setModules(it);
-                }
-            });
-        return () => subscribed = false;
-    }, [sceneId, appId, boundaryName]);
-
-
-    let selector;
-    if (modules && modules.length > 0) {
-        selector = <>
-            <>
-                <div className="p-field p-col-12 p-md-4 mr-4">
-                    <span className="p-float-label">
-                        <Dropdown id="modules"
-                                  optionLabel="name"
-                                  optionValue="name"
-                                  options={modules}
-                                  value={module}
-                                  showClear={true}
-                                  onChange={(e) => {
-                                      setModule(e.value);
-                                      onChange(e);
-                                  }}/>
-                        <label htmlFor="modules">Module</label>
-                    </span>
-                </div>
-            </>
-        </>
-    } else {
-        selector = <>
-        </>
-    }
-
-    return <>{selector}</>;
-}
+import {ModuleSelector} from "./ModuleSelector";
 
 export function Hotspots({sceneId, appId, date}) {
     const [dateRange, setDateRange] = useState(date);
