b54f7331a4d72cb9872c94eff8f1509d27114e37
Fouad Hamdi <1621862+fouadh@users.noreply.github.com>
2022-09-16 09:10:06 +0200

    hide dev networks tab

--- app/ui/src/screens/AppDetails.js
+++ app/ui/src/screens/AppDetails.js
@@ -14,7 +14,6 @@ import {EntityCoupling} from "./EntityCoupling";
 import {EntityContributions} from "./EntityContributions";
 import {CoordinationNeeds} from "./CoordinationNeeds";
 import {CodeAge} from "./CodeAge";
-import {DevNetwork} from "./DevNetwork";
 
 function useQuery() {
     return new URLSearchParams(useLocation().search);
@@ -90,9 +89,9 @@ export function AppDetails() {
                 <TabPanel header="Entity Contributions">
                     <EntityContributions sceneId={sceneId} appId={appId} date={dateRange} entities={entities} />
                 </TabPanel>
-                <TabPanel header="Developers Network">
-                    <DevNetwork sceneId={sceneId} appId={appId} date={dateRange}/>
-                </TabPanel>
+                {/*<TabPanel header="Developers Network">*/}
+                {/*    <DevNetwork sceneId={sceneId} appId={appId} date={dateRange}/>*/}
+                {/*</TabPanel>*/}
             </TabView>
         </>
     );
