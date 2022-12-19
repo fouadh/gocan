abde2135d2969347746d9786b3e7f92c29c745e0
Fouad Hamdi <1621862+fouadh@users.noreply.github.com@>
2022-12-19 15:48:46 +0100

    use the react 18 way to render the app

--- app/ui/src/index.js
+++ app/ui/src/index.js
@@ -1,20 +1,19 @@
 import React from 'react';
-import ReactDOM from 'react-dom';
+import {createRoot} from 'react-dom/client'
 import './index.css';
 import App from './App';
 import reportWebVitals from './reportWebVitals';
 
 if (process.env.NODE_ENV === 'development') {
-    const { worker } = require('./mocks/browser')
-    worker.start()
+  const {worker} = require('./mocks/browser')
+  worker.start()
 }
 
-ReactDOM.render(
-  <React.StrictMode>
-    <App />
-  </React.StrictMode>,
-  document.getElementById('root')
-);
+const container = document.getElementById('root')
+const root = createRoot(container)
+root.render(<React.StrictMode>
+  <App/>
+</React.StrictMode>)
 
 // If you want to start measuring performance in your app, pass a function
 // to log results (for example: reportWebVitals(console.log))
