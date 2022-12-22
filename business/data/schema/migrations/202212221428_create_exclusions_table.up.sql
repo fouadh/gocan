a46998d5e225a9a6dd0ef3d1bb258786596b2162
Fouad Hamdi <1621862+fouadh@users.noreply.github.com@>
2022-12-22 15:39:54 +0100

    add cascade deletion on exclusions

--- business/data/schema/migrations/202212221428_create_exclusions_table.up.sql
+++ business/data/schema/migrations/202212221428_create_exclusions_table.up.sql
@@ -1,5 +1,7 @@
 create table exclusions(
-    app_id uuid not null,
+    app_id uuid not null constraint fk_exclusions_app
+        references apps
+        on delete cascade,
     exclusion varchar(255) not null,
     primary key(app_id, exclusion)
 )
